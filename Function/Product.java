ef9bdcbd4e01093d0af7967cd36ec6ba2aebd719
Bantyb792 <bantyb792@gmail.com>
2024-08-16 22:59:50 +0530

    Create Product.java

---
+++ Function/Product.java
@@ -0,0 +1,14 @@
+public class Product {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String args[]) {
+        int a = 90;
+        int b = 45;
+        int prod = multiply(a, b);
+        System.out.println(prod);
+
+    }
+
+}
