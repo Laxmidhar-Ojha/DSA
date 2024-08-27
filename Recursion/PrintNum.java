2423a5396228f877f52c4ced8578b2eb9237888d
Bantyb792 <bantyb792@gmail.com>
2024-08-27 23:43:44 +0530

    Create PrintNum.java

---
+++ Recursion/PrintNum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class PrintNum {
+
+    public static void print(int n) {
+        if (n >= 0) {
+            System.out.println(n);
+            print(n - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+
+    }
+}
