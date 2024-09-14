0d6387bdbe10c5391dd56d6533aea3aff051a412
Bantyb792 <bantyb792@gmail.com>
2024-09-14 20:23:17 +0530

    Add files via upload

---
+++ Queue/DequeB.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class DequeB {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        System.out.println(deque);
+
+    }
+
+}
