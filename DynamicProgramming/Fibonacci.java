7b1e518b3f63a4ee0f8d4b430b55d8788e2978d9
Laxmidhar Ojha <bantyb792@gmail.com>
2024-09-30 22:36:03 +0530

    Create Fibonacci.java

---
+++ DynamicProgramming/Fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Fibonacci { // memoization
+    public static int fib(int n, int dp[]) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+        return dp[n];
+    }
+
+    public static int fibTabulation(int n) { // tabulation
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int dp[] = new int[n + 1];
+        System.out.println(fibTabulation(n));
+    }
+}
